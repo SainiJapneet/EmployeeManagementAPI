754ef979402098f50a1ea60f2aba2872894d8114
japneet <japneetsaini084@gmail.com>
2023-07-06 19:25:56 +0530

    Initial commit

---
+++ routes/routes.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import Employee from "../model/employeeModel.mjs"
+
+const route = express.Router();
+
+//Add employee
+route.get()
+
+//Get employee
+
+
+//Get employee by ID
+
+
+//Update employee by ID
+
+
+//Delete employee by ID
