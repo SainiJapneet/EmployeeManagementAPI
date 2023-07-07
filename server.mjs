9fc06d847eeda4bffc721b13855feed528fdc8b6
japneet <japneetsaini084@gmail.com>
2023-07-07 19:19:26 +0530

    cors error handling for frontend

--- server.mjs
+++ server.mjs
@@ -5,6 +5,16 @@ import routes from "./routes/routes.mjs";
 const app = express();
 const port = 4040;
 dbConnect();
+
+//cors error handling 
+app.use((request, response, next) => {
+    response.setHeader("Access-Control-Allow-Origin","*");
+    response.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Content-Type, Authorization");
+    response.setHeader("Access-Control-Allow-Methods","POST, GET, PATCH, PUT, DELETE, OPTIONS");
+    next();
+})
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api',routes);
